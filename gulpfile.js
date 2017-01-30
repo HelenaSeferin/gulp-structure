b4a2f72babeba11e8849eb294ab9de29b5e14314
amandasv <amandasv.pm@gmail.com>
2017-01-30 18:55:30 -0200

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var target          = 'dist/';
+var target          = 'public/';
 var gulp            = require('gulp');
 var path            = require('path');
 var rev             = require('gulp-rev');
@@ -6,10 +6,11 @@ var sass            = require('gulp-sass');
 var uglify          = require('gulp-uglify');
 var concat          = require('gulp-concat');
 var rename          = require('gulp-rename');
-var imagemin        = require('gulp-imagemin');
 var uglifycss       = require('gulp-uglifycss');
 var sourcemaps      = require('gulp-sourcemaps');
 var browserSync     = require('browser-sync').create();
+
+gulp.task('build', ['css-rev', 'js-rev']);
 
 gulp.task('serve', ['build'], function () {
 
@@ -18,19 +19,16 @@ gulp.task('serve', ['build'], function () {
     });
 
     gulp.watch('src/scss/**/*.scss', ['css-compile']);
-    gulp.watch('src/js/*.js');
+    gulp.watch('src/js/*.js', ['js-compile']);
     gulp.watch('src/**/*.html').on('change', browserSync.reload);
 
 });
 
-gulp.task('build', ['html', 'css-rev', 'js-rev']);
-
 gulp.task('css-compile', function() {
-  return gulp.src('src/scss/**/*.scss')
+  return gulp.src(['src/scss/**/*.scss','!src/scss/**/_*.scss'])
     .pipe(sass())
     .pipe(concat('style.css'))
-    .pipe(gulp.dest(target + 'css'))
-    /*.pipe(browserSync.stream())*/;
+    .pipe(gulp.dest(target + 'css'));
 });
 
 gulp.task('css-minify', ['css-compile'], function() {
@@ -40,6 +38,16 @@ gulp.task('css-minify', ['css-compile'], function() {
       path.extname = '.min.css';
     }))
     .pipe(gulp.dest(target + 'css'));
+});
+
+gulp.task('css-rev', ['css-minify'], function () {
+  return gulp.src([target + 'css/*.css'], {base: path.join(process.cwd(), 'public')})
+    .pipe(rev())
+    .pipe(rev.manifest(target+'rev-manifest.json', {
+      base: target,
+      merge: true,
+    }))
+    .pipe(gulp.dest(target));
 });
 
 gulp.task('js-compile', function() {
@@ -57,47 +65,14 @@ gulp.task('js-minify', ['js-compile'], function() {
     .pipe(gulp.dest(target + 'js'))
 });
 
-gulp.task('html', function() {
-  return gulp.src('src/*.html')
-    .pipe(gulp.dest(target))
-});
-
 gulp.task('js-rev', ['js-minify'], function () {
-  return gulp.src([target + 'js/*.min.js'])
+  return gulp.src([target + 'js/*.js'], {base: path.join(process.cwd(), 'public')})
     .pipe(rev())
     .pipe(rev.manifest(target+'rev-manifest.json', {
       base: target,
       merge: true,
     }))
     .pipe(gulp.dest(target));
-});
-
-gulp.task('css-rev', ['css-minify'], function () {
-  return gulp.src([target + 'css/*.min.css'])
-    .pipe(rev())
-    .pipe(rev.manifest(target+'rev-manifest.json', {
-      base: target,
-      merge: true,
-    }))
-    .pipe(gulp.dest(target));
-});
-
-gulp.task('image-minify', function () {
-  return gulp.src('src/images/**/*')
-    .pipe(imagemin({
-      progressive: true,
-      interlaced: true
-    }))
-    .pipe(gulp.dest(target + 'img'));
-});
-
-gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-      baseDir: './'
-    },
-    startPath: '/html'
-  });
 });
 
 gulp.task('default', ['serve']);
